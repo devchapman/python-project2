9b5d95b45167a2cb32fbc4481018f7366dfc8b85
Deven Chapman <devchapman1911@gmail.com>
2023-07-30 14:45:34 -0400

    stash

--- python project.py
+++ python project.py
@@ -2,14 +2,14 @@ import random
 
 class Garage():
     def __init__(self):
-        self.tickets = ['1', '2', '3', '4','5']
+        self.tickets = ['1', '2', '3', '4', '5']
         self.parkingspace = ['1','2','3', '4', '5']
         self.currentTicket = ticket_info = {"paid": False}
 
     def takeTicket(self):
         self.tickets -= 1
         parkingspace -= 1
-        pass
+        
 
     def payForParking(self):
         total_amount = input("How much are you paying for parking:")
@@ -19,6 +19,10 @@ class Garage():
 
     
     def leaveGarage(self):
-        if self.currentTicket['paid'] == True
+        if self.currentTicket['paid'] == True:
+            parkingspace += 1
             print("Thank You have a nice day!")
-        else: 
+        
+        else: self.currentTicket['paid'] == False
+        parkingspace -= 1 
+        print("You still need to pay!")
